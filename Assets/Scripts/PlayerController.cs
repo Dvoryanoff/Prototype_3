68933c805fb0c8e7579b8d5a03f0f4ddc3257442
Dvoryanoff <dvoryanoff@mail.ru>
2022-07-15 20:53:36 +0300

    Add Particles Effects(music and sounds)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     private Rigidbody playerRb;
     private Animator playerAnimator;
+    private AudioSource playerAudioSource;
+
+    [SerializeField] private ParticleSystem explosionParticle;
+    [SerializeField] private ParticleSystem dirtParticle;
     [SerializeField] private float gravityModifier;
     [SerializeField] private bool isOnGround = true;
+
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip crashSound;
 
     //public static event Action GameOver;
 
@@ -19,6 +26,7 @@ public class PlayerController : MonoBehaviour {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnimator = GetComponent<Animator>();
+        playerAudioSource = GetComponent<AudioSource>();
         //Jump();
     }
 
@@ -31,15 +39,20 @@ public class PlayerController : MonoBehaviour {
     private void Jump() {
         playerRb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
         playerAnimator.SetTrigger("Jump_trig");
+        playerAudioSource.PlayOneShot(jumpSound, 1.0f);
+        dirtParticle.Stop();
         isOnGround = false;
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Ground")) {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle")) {
             Loose();
             Death();
+            playerAudioSource.PlayOneShot(crashSound, 1.0f);
+
         }
     }
 
@@ -51,5 +64,8 @@ public class PlayerController : MonoBehaviour {
     private void Death() {
         playerAnimator.SetBool("Death_b", true);
         playerAnimator.SetInteger("DeathType_int", 1);
+        explosionParticle.Play();
+        dirtParticle.Stop();
     }
+
 }
